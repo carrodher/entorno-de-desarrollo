368cd620176e5e82dc7c05fbaf14bfc29476d57a
Carlos Rodriguez Hernandez <crhernandez@bitnami.com>
2019-09-01 16:50:28 +0200

    Modify gitignore and set everytime git username/mail

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Configure git with the proper mail and user
-if [[ -f ~/.gitconfig ]]; then
-  printf '[initialization] Git configuration found, using it...\n'
+if [[ $USER_EMAIL && $USER_NAME ]]; then
+  printf '[initialization] Configuring Git with email=%s and name=%s\n' "${USER_EMAIL}" "${USER_NAME}"
+  git config --global user.email "${USER_EMAIL}" && git config --global user.name "${USER_NAME}"
 else
-  if [[ $USER_EMAIL && $USER_NAME ]]; then
-    printf '[initialization] Configuring Git with email=%s and name=%s\n' "${USER_EMAIL}" "${USER_NAME}"
-    git config --global user.email "${USER_EMAIL}" && git config --global user.name "${USER_NAME}"
-  else
-    printf '[initialization] WARNING! Git not configured\n'
-  fi
+  printf '[initialization] WARNING! Git not configured\n'
 fi
 
 # Execute initialization scripts
