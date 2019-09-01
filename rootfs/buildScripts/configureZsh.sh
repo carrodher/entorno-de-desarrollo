0104cd3e1a2c068cfa47f6b71ab23e86713499a1
Carlos Rodriguez Hernandez <crhernandez@bitnami.com>
2019-09-01 12:17:18 +0200

    Change example files name and configure dump location zsh

--- rootfs/buildScripts/configureZsh.sh
+++ rootfs/buildScripts/configureZsh.sh
@@ -4,8 +4,9 @@ chsh -s "$(command -v zsh)"
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O install.sh
 chmod +x install.sh
 ./install.sh --unattended
+rm -rf ./install.sh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sed -i 's|ZSH_THEME="robbyrussell"|ZSH_THEME="avit"|g' ~/.zshrc
 sed -i 's|plugins=(git)|plugins=(git colored-man-pages zsh-autosuggestions zsh-syntax-highlighting)|g' ~/.zshrc
-rm -rf ./install.sh
+sed -i 's|-C -d "\${ZSH_COMPDUMP}"|-C -d "/tmp/.zcompdump-\${SHORT_HOST}-\${ZSH_VERSION}"|g' ~/.oh-my-zsh/oh-my-zsh.sh
